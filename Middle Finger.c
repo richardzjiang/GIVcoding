811e2f2c52735605d8ed87fbb22c5c2b2d30cc90
Richard Jiang <jiangr@sbschools.net>
2023-07-04 23:37:50 -0400

    C programs written at GIV Tech & Design

---
+++ Middle Finger.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+
+
+sleep(1);
+printf("\033[1;1H");
+printf("    .-.\n");
+printf("\033[2;1H");
+printf("    | |\n");
+printf("\033[3;1H");
+printf("    | |\n");
+printf("\033[4;1H");
+printf(" _.-| |-.\n");
+printf("\033[5;1H");
+printf(": | | | | ,-,\n");
+printf("\033[6;1H");
+printf("|-'-' '-|/ /\n");
+printf("\033[7;1H");
+printf("|     ,-' /\n");
+printf("\033[8;1H");
+printf("|    ;    |\n");
+printf("\033[9;1H");
+printf("|        /\n");
+printf("\033[10;1H");
+printf(" \\______/\n");
+printf("\033[11;1H");
+printf(" _   _  _   _   _  _ ___\n");
+printf("\033[12;1H");
+printf("|_| |_ |_  |_| |_ |   |\n");
+printf("\033[13;1H");
+printf("| \\ |_  _| |   |_ |_  |\n");
+
+while (1 == 1) {
+sleep(1);
+printf("\033[1;1H");
+printf("                          ");
+printf("\033[2;1H");
+printf("                   ");
+printf("\033[2;1H");
+printf("    .-.\n");
+
+sleep(1);
+printf("\033[2;1H");
+printf("                          ");
+printf("\033[3;1H");
+printf("                   ");
+printf("\033[3;1H");
+printf("    .-.\n");
+
+sleep(1);
+printf("\033[3;1H");
+printf("                          ");
+printf("\033[4;1H");
+printf("                   ");
+printf("\033[4;1H");
+printf(" _.-.-.-.\n");
+
+sleep(1);
+printf("\033[4;1H");
+printf("                          ");
+printf("\033[4;1H");
+printf(" _.-| |-.\n");
+printf("\033[3;1H");
+printf("    .-.\n");
+
+sleep(1);
+printf("\033[3;1H");
+printf("                          ");
+printf("\033[3;1H");
+printf("    | |\n");
+printf("\033[2;1H");
+printf("    .-.\n");
+
+sleep(1);
+printf("\033[2;1H");
+printf("                          ");
+printf("\033[2;1H");
+printf("    | |\n");
+printf("\033[1;1H");
+printf("    .-.\n");
+
+}
+}
